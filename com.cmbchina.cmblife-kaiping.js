111b116a5715ef563db8a559cf84147928165feb
gitxrf <144418082+gitxrf@users.noreply.github.com>
2023-11-22 16:32:13 +0800

    Create com.cmbchina.cmblife-kaiping.js

---
+++ com.cmbchina.cmblife-kaiping.js
@@ -0,0 +1,11 @@
+/***********************************************
+> 应用名称：掌上生活
+> 应用功能：开屏广告
+> 发布时间：2023-11-22
+> 更新时间：2023-11-22
+[rewrite_local]
+^https:\/\/az\dmbasecc\.bas\.cmbchina\.com\/Edge\/api\/mlife\.clientface\.clientservice\.api\.advertiseService\/preCacheAdvertiseSec url reject-dict
+
+[mitm]
+hostname = az*mbasecc.bas.cmbchina.com
+***********************************************/
